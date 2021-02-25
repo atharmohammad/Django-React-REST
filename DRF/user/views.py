0cba1f8e6434c676ac01c69294de6406efbb86ae
atharmohammad <mohd.rule123@gmail.com>
2021-02-25 14:29:04 +0530

    Logout view return response

--- DRF/user/views.py
+++ DRF/user/views.py
@@ -28,5 +28,6 @@ class BlackListTokenView(APIView):
             refresh_token = request.data['refresh_token']
             token = RefreshToken(refresh_token)
             token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status = status.HTTP_400_BAD_REQUEST)
